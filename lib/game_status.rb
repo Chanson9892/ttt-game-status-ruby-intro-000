1f48fc13f7f21c63ce8f8679328628c9c070102b
Chandler Hanson <chan.hanson5@gmail.com>
2020-06-20 02:40:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,8 +30,6 @@ end
 def draw?(board)
    if !won?(board) && full?(board)
      return true
-   elsif !won?(board) && !full?(board)
-     return false
    else won?(board)
      return false
    end
